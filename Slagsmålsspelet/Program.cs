86c3bc05ecb576ec1423aef57fc67ecc142b9c4e
Nils Berglund <nilsberglund04@gmail.com>
2021-10-12 10:58:09 +0200

    oifdx

--- Slagsmålsspelet/Program.cs
+++ Slagsmålsspelet/Program.cs
@@ -4,11 +4,6 @@ int P1HP = 100;
 
 int P2HP = 100;
 
-Random generator = new Random();
-
-int damage = generator.Next(10, 30);
-int damage2 = generator.Next(10, 30);
-
 Console.WriteLine("P1 chose your name");
 string name;
 name = Console.ReadLine();
@@ -24,8 +19,13 @@ Console.WriteLine("Press Enter To Atack");
 
 Console.ReadLine(); 
 
-do
+while (P1HP > 0 && P2HP > 0)
 {
+    Random generator = new Random();
+
+    int damage = generator.Next(10, 30);
+    int damage2 = generator.Next(10, 30);
+
     P1HP = P1HP-damage2;
     P2HP= P2HP-damage;
 
@@ -36,15 +36,18 @@ do
     Console.ReadLine();
 }
 
-while (P1HP > 0 || P2HP > 0);
+if (P1HP < 0 && P2HP <0)
+{
+    Console.WriteLine("DRAW, BOTH MATCH EACH OTHERS POWER!!!");
+}
 
-if (P1HP < 0)
+else if (P1HP < 0)
 {
     Console.WriteLine(name2 + " has beaten " + name + " to a pulp");
 }
-else if (P2HP <0)
+else if (P2HP < 0)
 {
     Console.WriteLine(name + " has beaten P2 " + name2 + " to a pulp");
 }
 
-Console.ReadLine();
+Console.ReadLine(); 
